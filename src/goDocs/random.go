f38455f1e472792e1008976c815df084271de174
Justin Montgomery <montgomerycodepro@gmail.com>
2020-08-31 19:18:04 -0400

    more to the git ignore

--- src/goDocs/random.go
+++ src/goDocs/random.go
@@ -13,6 +13,8 @@ func main() {
 	fmt.Println(rand.Intn(10))
 }
 
+
+
 /*
  using the rand package of math gives you a pseudorandom num. To really get a random number you need to seed with an ever changing value.
 Like how we are seeding with current time it is always changing.
